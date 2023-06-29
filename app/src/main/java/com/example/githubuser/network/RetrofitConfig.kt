82eec0f98dd7fab4ac1a891abeabaa2c9c5d5d8b
abyanhmd <abyan806@gmail.com>
2023-06-29 12:36:20 +0700

    Indecent Exposure - Remove exposed token
    The API key is hardcoded directly in the class. This is not recommended as it exposes sensitive information and makes it difficult to manage or update the key in the future. It would be better to use a more secure approach to handle the API key. One of the solution to hide the API key is using environment variables, in this case local.properties. Also make sure local.properties is included in .gitignore to prevent the it from uploaded to the github. This way, the API key will not be exposed and prevent another party to use the token exposed in the class.

--- app/src/main/java/com/example/githubuser/network/RetrofitConfig.kt
+++ app/src/main/java/com/example/githubuser/network/RetrofitConfig.kt
@@ -1,20 +1,27 @@
 package com.example.githubuser.network
 
+import android.util.Log
+import com.example.githubuser.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
 
 class RetrofitConfig {
     companion object {
         fun getUserService(): UserService {
+            val apiKey = BuildConfig.API_TOKEN
+
             val loggingInterceptor =
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
             val authInterceptor = Interceptor { chain ->
                 val request = chain.request().newBuilder()
-                    .addHeader("Authorization", "token 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")
+                    .addHeader("Authorization", "token $apiKey")
                     .build()
                 chain.proceed(request)
             }
